9986a3169c9bc6adfe26f7285f6a2a4feabd6793
Otavio Guilherme <taviogui@gmail.com>
2018-07-01 22:44:38 -0300

    bd para teste

---
+++ app/server/bd_Test/teste_db.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.7.9
+ */
+
+/**
+ * Database `teste_db`
+ */
+
+/* `teste_db`.`cadastro` */
+$cadastro = array(
+  array('id' => '1','nome' => 'sahush','sobre_nome' => 'usuahush','email' => 'uhsuahsuha','login' => 'suahsuh','senha' => 'hsauhs'),
+  array('id' => '2','nome' => 'shaushuh','sobre_nome' => 'uhsauhsuha','email' => 'hsuahsh','login' => 'uhsahs','senha' => 'hsuahas'),
+  array('id' => '3','nome' => 'hsausha','sobre_nome' => 'uhsuahs','email' => 'hsuahush','login' => 'uhsauhsa','senha' => 'uhsauhsa'),
+  array('id' => '4','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '5','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '6','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '7','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '8','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '9','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '10','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '11','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '12','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '13','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '14','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '15','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '16','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '17','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '18','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '19','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '20','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '21','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '22','nome' => 'shaush','sobre_nome' => 'uhsauhsuah','email' => 'uhsuahsuh','login' => 'usauhsah','senha' => 'ushauhsa'),
+  array('id' => '23','nome' => 'wahwuah','sobre_nome' => 'hushauh','email' => 'hsauhsu','login' => 'hushauhs','senha' => 'hsuahs'),
+  array('id' => '24','nome' => 'sajusj','sobre_nome' => 'ijsijaisj','email' => 'ijsiajs','login' => 'ijsiaj','senha' => 'jsja'),
+  array('id' => '25','nome' => 'hsauhsua','sobre_nome' => 'hsuahs','email' => 'hushauhs','login' => 'hsauhsu','senha' => 'uhsauhs'),
+  array('id' => '26','nome' => 'hsauhsua','sobre_nome' => 'hsuahs','email' => 'hushauhs','login' => 'hsauhsu','senha' => 'uhsauhs'),
+  array('id' => '27','nome' => 'kkkkkk','sobre_nome' => 'kkkk','email' => 'kkk','login' => 'kkkk','senha' => 'kkk'),
+  array('id' => '28','nome' => 'kkkkkk','sobre_nome' => 'kkkk','email' => 'kkk','login' => 'kkkk','senha' => 'kkk'),
+  array('id' => '29','nome' => 'kkkkkk','sobre_nome' => 'kkkk','email' => 'kkk','login' => 'kkkk','senha' => 'kkk')
+);
